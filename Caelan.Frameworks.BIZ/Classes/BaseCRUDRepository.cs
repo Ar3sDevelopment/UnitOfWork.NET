99906794392202c89ae507356e3efb4d7b7fb9e2
Matteo Bruni <Ar3sHack@gmail.com>
2014-06-23 16:55:22 +0200

    Virtualized CRUD methods

--- Caelan.Frameworks.BIZ/Classes/BaseCRUDRepository.cs
+++ Caelan.Frameworks.BIZ/Classes/BaseCRUDRepository.cs
@@ -1,26 +1,42 @@
 using System;
+using System.Linq;
 using Caelan.Frameworks.BIZ.Interfaces;
 using Caelan.Frameworks.DAL.Interfaces;
 
 namespace Caelan.Frameworks.BIZ.Classes
 {
-    public abstract class BaseCRUDRepository<TEntity, TDTO, TKey> : BaseRepository<TEntity, TDTO, TKey>, IInsertRepository<TDTO>, IUpdateRepository<TDTO>, IDeleteRepository<TDTO>
-        where TEntity : class, IEntity<TKey>, new()
-        where TDTO : class, IDTO<TKey>, new()
-        where TKey : IEquatable<TKey>
-    {
-        protected BaseCRUDRepository(BaseUnitOfWork manager)
-            : base(manager)
-        {
-        }
+	public abstract class BaseCRUDRepository<TEntity, TDTO, TKey> : BaseRepository<TEntity, TDTO, TKey>, IInsertRepository<TDTO>, IUpdateRepository<TDTO>, IDeleteRepository<TDTO>
+		where TEntity : class, IEntity<TKey>, new()
+		where TDTO : class, IDTO<TKey>, new()
+		where TKey : IEquatable<TKey>
+	{
+		protected BaseCRUDRepository(BaseUnitOfWork manager)
+			: base(manager)
+		{
+		}
 
-        public abstract void Insert(TDTO dto);
-        public abstract void Update(TDTO dto);
-        public abstract void Delete(TDTO dto);
+		public virtual void Insert(TDTO dto)
+		{
+			All().Add(EntityBuilder().Build(dto));
+		}
 
-        public virtual void Delete(TKey id)
-        {
-            Delete(Single(id));
-        }
-    }
+		public virtual void Update(TDTO dto)
+		{
+			var entity = All().FirstOrDefault(t => t.ID.Equals(dto.ID));
+
+			EntityBuilder().Build(dto, ref entity);
+		}
+
+		public virtual void Delete(TDTO dto)
+		{
+			var entity = All().FirstOrDefault(t => t.ID.Equals(dto.ID));
+
+			All().Remove(entity);
+		}
+
+		public virtual void Delete(TKey id)
+		{
+			Delete(Single(id));
+		}
+	}
 }
