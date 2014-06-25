411ddf43bc0692021735a2ccdb58fd7c6c25db77
Matteo Bruni <matteo.bruni@me.com>
2014-06-25 20:42:33 +0200

    Fix for ref

--- Caelan.Frameworks.BIZ/Classes.fs
+++ Caelan.Frameworks.BIZ/Classes.fs
@@ -164,10 +164,10 @@
         default this.Update(dto : 'TDTO) =
             let entity =
                 match this.Set() |> Seq.tryFind (fun t -> t.ID = dto.ID) with
-                | None -> null
-                | Some(value) -> value
+                | None -> ref null
+                | Some(value) -> ref value
 
-            this.EntityBuilder().Build(dto, ref entity)
+            this.EntityBuilder().Build(dto, entity)
 
         abstract member Delete : 'TDTO -> unit
         default this.Delete(dto : 'TDTO) =
