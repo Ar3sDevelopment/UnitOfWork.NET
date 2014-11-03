83abf0ca280f58a142a40c92a54f6912a2f7a1f1
Matteo Bruni <Ar3sHack@gmail.com>
2014-11-03 16:47:10 +0100

    Added Test Project

---
+++ Caelan.Frameworks.BIZ.Tests/BusinessTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Caelan.Frameworks.BIZ.Tests
+{
+	[TestClass]
+	public class BusinessTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
