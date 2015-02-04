21309fa3eea4075df9e3d8ac3319d82c2cccb97c
Matteo Bruni <ar3shack@gmail.com>
2015-02-04 11:22:35 +0100

    Added NUnit project

---
+++ Caelan.Frameworks.BIZ.NUnit/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Caelan.Frameworks.BIZ.NUnit
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
