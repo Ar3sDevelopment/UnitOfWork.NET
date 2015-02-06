48ac027be8b3c114c152547e61a0e8ad2347f2fc
Matteo Bruni <ar3shack@gmail.com>
2015-02-06 20:53:18 +0100

    Code refactoring, now smaller and more readable files. Added a RepositoryReflection module for mantain reflection code away from UoW

---
+++ Caelan.Frameworks.BIZ/RepositoryDTO.fs
@@ -0,0 +1,100 @@
+﻿namespace Caelan.Frameworks.BIZ.Classes
+
+open System
+open System.Data.Linq
+open System.Linq
+open System.Linq.Expressions
+open System.Reflection
+open Caelan.DynamicLinq.Classes
+open Caelan.DynamicLinq.Extensions
+open Caelan.Frameworks.Common.Interfaces
+open Caelan.Frameworks.Common.Classes
+open Caelan.Frameworks.BIZ.Interfaces
+
+[<AllowNullLiteral>]
+type Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct>(manager) = 
+    inherit Repository<'TEntity>(manager : IUnitOfWork)
+    
+    interface IRepository<'TEntity, 'TDTO> with
+        member this.DTOBuilder(mapper) = mapper |> this.DTOBuilder
+        member this.EntityBuilder(mapper) = mapper |> this.EntityBuilder
+        member this.DTOBuilder() = this.DTOBuilder()
+        member this.EntityBuilder() = this.EntityBuilder()
+        member this.SingleDTO([<ParamArray>] ids : obj []) = ids |> this.SingleDTO
+        member this.SingleDTO(expr : Expression<Func<'TEntity, bool>>) = expr |> this.SingleDTO
+        member this.List() = this.List()
+        member this.List whereExpr = whereExpr |> this.List
+        member this.All(take, skip, sort, filter, whereFunc) = this.All(take, skip, sort, filter, whereFunc)
+        member this.Insert(dto : 'TDTO) = dto |> this.Insert
+        member this.Update(dto : 'TDTO, [<ParamArray>] ids) = this.Update(dto, ids)
+        member this.Delete(dto : 'TDTO, [<ParamArray>] ids) = this.Delete(dto, ids)
+    
+    member val DTOMapper : IMapper<'TEntity, 'TDTO> = null with get, set
+    member val EntityMapper : IMapper<'TDTO, 'TEntity> = null with get, set
+    member this.DTOBuilder() = this.DTOMapper |> this.DTOBuilder
+    member this.EntityBuilder() = this.EntityMapper |> this.EntityBuilder
+    
+    member __.DTOBuilder mapper = 
+        match mapper with
+        | null -> Builder.Source<'TEntity>().Destination<'TDTO>()
+        | _ -> Builder.Source<'TEntity>().Destination<'TDTO> mapper
+    
+    member __.EntityBuilder mapper = 
+        match mapper with
+        | null -> Builder.Source<'TDTO>().Destination<'TEntity>()
+        | _ -> Builder.Source<'TDTO>().Destination<'TEntity> mapper
+    
+    member this.SingleDTO([<ParamArray>] ids : obj []) = 
+        match this.SingleEntity(ids) with
+        | null -> null
+        | entity -> entity |> this.DTOBuilder().Build
+    
+    member this.SingleDTO(expr : Expression<Func<'TEntity, bool>>) = 
+        match this.SingleEntity(expr) with
+        | null -> null
+        | entity -> entity |> this.DTOBuilder().Build
+    
+    member this.List() = this.DTOBuilder().BuildList(this.All() :> seq<'TEntity>)
+    member this.List(whereExpr) = this.DTOBuilder().BuildList(this.All(whereExpr) :> seq<'TEntity>)
+    member this.All(take, skip, sort, filter, whereFunc) = 
+        this.All(take, skip, sort, filter, whereFunc, this.DTOBuilder().BuildList)
+    
+    member private this.All(take, skip, sort, filter, whereFunc, buildFunc : seq<'TEntity> -> seq<'TDTO>) = 
+        let orderBy = 
+            query { 
+                for item in (typeof<'TEntity>).GetProperties(BindingFlags.Instance ||| BindingFlags.Public) 
+                            |> Seq.map (fun t -> t.Name) do
+                    select item
+                    headOrDefault
+            }
+        
+        let queryResult = 
+            (match orderBy with
+             | null -> this.All(whereFunc)
+             | defaultSort -> this.All(whereFunc).OrderBy(defaultSort)).ToDataSourceResult(take, skip, sort, filter)
+        
+        DataSourceResult<_>(Data = buildFunc (queryResult.Data), Total = queryResult.Total)
+    
+    abstract Insert : dto:'TDTO -> 'TDTO
+    abstract Update : 'TDTO * ids:obj [] -> unit
+    abstract Delete : 'TDTO * ids:obj [] -> unit
+    override this.Insert(dto : 'TDTO) = this.DTOBuilder().Build(this.Set().Add(this.EntityBuilder().Build(dto)))
+    
+    override this.Update(dto : 'TDTO, [<ParamArray>] ids) = 
+        let entity = this.Set().Find(ids)
+        let entry = manager.Entry(entity)
+        this.EntityBuilder().Build(dto, ref entity)
+        entry.CurrentValues.SetValues(entity)
+    
+    override this.Delete(_ : 'TDTO, [<ParamArray>] ids) = this.Delete(ids) |> ignore
+    member this.InsertAsync(dto : 'TDTO) = async { return this.Insert(dto) } |> Async.StartAsTask
+    member this.UpdateAsync(dto : 'TDTO, ids) = async { this.Update(dto, ids) } |> Async.StartAsTask
+    member this.DeleteAsync(dto : 'TDTO, [<ParamArray>] ids) = async { this.Delete(dto, ids) } |> Async.StartAsTask
+    member this.ListAsync(whereExpr : Expression<Func<'TEntity, bool>>) = 
+        async { return this.List(whereExpr) } |> Async.StartAsTask
+    member this.ListAsync() = async { return this.List() } |> Async.StartAsTask
+    member this.AllAsync(take : int, skip : int, sort : seq<Sort>, filter : Filter, 
+                         whereFunc : Expression<Func<'TEntity, bool>>) = 
+        async { return this.All(take, skip, sort, filter, whereFunc) } |> Async.StartAsTask
+    member this.SingleDTOAsync([<ParamArray>] id : obj []) = async { return this.SingleDTO(id) } |> Async.StartAsTask
+    member this.SingleDTOAsync(expr : 'TEntity -> bool) = async { return this.SingleDTO(expr) } |> Async.StartAsTask
--- Caelan.Frameworks.BIZ/UnitOfWork.fs
+++ Caelan.Frameworks.BIZ/UnitOfWork.fs
@@ -7,57 +7,10 @@ open System.Reflection
 open System.Collections.Generic
 open Caelan.Frameworks.BIZ.Interfaces
 open Caelan.Frameworks.Common.Helpers
+open Caelan.Frameworks.BIZ.Modules
 
 [<AllowNullLiteral>]
 type UnitOfWork(context : DbContext) = 
-    
-    let findRepositoryInAssemblies ([<ParamArray>] args : obj []) (baseType : Type) = 
-        let typeEqualsTo = (fun t1 t2 -> t2 = t1)
-        let isTypeAssignableTo = (fun t1 (t2 : Type) -> t2.IsAssignableFrom(t1))
-        let typeSameGeneric = 
-            (fun (t1 : Type) (t2 : Type) -> 
-            t1.IsGenericTypeDefinition && t1.GetGenericArguments().Length = t2.GenericTypeArguments.Length)
-        
-        let makeGenericSafe (t : Type) types = 
-            try 
-                t.MakeGenericType(types)
-            with _ -> null
-        
-        let rec compareTypes comparer (type1 : Type) (type2 : Type) = 
-            match type1 with
-            | null when type2 <> null -> false
-            | null when type2 = null -> true
-            | _ when (type1, type2) ||> comparer -> true
-            | _ when (type1, type2) ||> typeSameGeneric -> 
-                ((type1, type2.GenericTypeArguments) ||> makeGenericSafe, type2) ||> compareTypes comparer
-            | _ when (type2, type1) ||> typeSameGeneric -> 
-                (type1, (type2, type1.GenericTypeArguments) ||> makeGenericSafe) ||> compareTypes comparer
-            | _ -> false
-        
-        let findRepositoryInAssembly (assembly : Assembly) = 
-            let types = assembly.GetTypes() |> Seq.filter (fun t -> not t.IsInterface && not t.IsAbstract)
-            match types |> Seq.tryFind (fun t -> (t, baseType) ||> compareTypes typeEqualsTo) with
-            | None -> types |> Seq.tryFind (fun t -> (t, baseType) ||> compareTypes isTypeAssignableTo)
-            | t -> t
-        
-        let rec getRepository asmList = 
-            match asmList with
-            | head :: tail -> 
-                match head |> findRepositoryInAssembly with
-                | None -> tail |> getRepository
-                | Some(repo) when repo.ContainsGenericParameters -> repo.MakeGenericType(baseType.GenericTypeArguments)
-                | Some(repo) -> repo
-            | [] -> null
-        
-        let repoType = 
-            [ Assembly.GetExecutingAssembly()
-              Assembly.GetEntryAssembly()
-              Assembly.GetCallingAssembly() ]
-            |> List.filter (fun t -> t <> null)
-            |> getRepository
-        
-        Activator.CreateInstance(repoType, args) :?> IRepository
-    
     interface IUnitOfWork with
         member this.SaveChanges() = this.SaveChanges()
         member this.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
@@ -73,6 +26,9 @@ type UnitOfWork(context : DbContext) =
             this.Repository<'TEntity, 'TDTO, 'TListDTO>()
         member this.Transaction(body : Action<IUnitOfWork>) = this.Transaction(body)
         member this.TransactionSaveChanges(body : Action<IUnitOfWork>) = this.TransactionSaveChanges(body)
+
+    interface IDisposable with
+        member this.Dispose() = this.Dispose()
     
     member __.SaveChanges() = context.SaveChanges()
     member __.SaveChangesAsync() = async { return! context.SaveChangesAsync() |> Async.AwaitTask } |> Async.StartAsTask
@@ -86,14 +42,13 @@ type UnitOfWork(context : DbContext) =
     
     member this.Repository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
         typeof<IRepository<'TEntity>> 
-        |> MemoizeHelper.Memoize(fun t -> t |> findRepositoryInAssemblies [| this |] :?> IRepository<'TEntity>)
+        |> MemoizeHelper.Memoize (fun t -> t |> RepositoryReflection.FindRepositoryInAssemblies [| this |] :?> IRepository<'TEntity>)
     member this.Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct>() = 
         typeof<IRepository<'TEntity, 'TDTO>> 
-        |> MemoizeHelper.Memoize(fun t -> t |> findRepositoryInAssemblies [| this |] :?> IRepository<'TEntity, 'TDTO>)
+        |> MemoizeHelper.Memoize (fun t -> t |> RepositoryReflection.FindRepositoryInAssemblies [| this |] :?> IRepository<'TEntity, 'TDTO>)
     member this.Repository<'TEntity, 'TDTO, 'TListDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct and 'TListDTO : equality and 'TListDTO : null and 'TListDTO : not struct>() = 
         typeof<IListRepository<'TEntity, 'TDTO, 'TListDTO>> 
-        |> MemoizeHelper.Memoize
-               (fun t -> t |> findRepositoryInAssemblies [| this |] :?> IListRepository<'TEntity, 'TDTO, 'TListDTO>)
+        |> MemoizeHelper.Memoize (fun t -> t |> RepositoryReflection.FindRepositoryInAssemblies [| this |] :?> IListRepository<'TEntity, 'TDTO, 'TListDTO>)
     member __.Entry<'TEntity>(entity : 'TEntity) = context.Entry(entity)
     member __.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
         context.Set<'TEntity>()
@@ -115,9 +70,6 @@ type UnitOfWork(context : DbContext) =
             with _ -> 
                 transaction.Rollback()
                 false)
-    
-    interface IDisposable with
-        member this.Dispose() = this.Dispose()
     
     abstract Dispose : unit -> unit
     override __.Dispose() = ()
