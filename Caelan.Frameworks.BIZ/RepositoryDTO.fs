a8fb8b41d4d0c46cf7175ee804436fdce38fd42b
Matteo Bruni <ar3shack@gmail.com>
2015-02-14 10:50:11 +0100

    Fix for dto update, reverted to older commit

--- Caelan.Frameworks.BIZ/RepositoryDTO.fs
+++ Caelan.Frameworks.BIZ/RepositoryDTO.fs
@@ -81,7 +81,10 @@ type Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equali
     override this.Insert(dto : 'TDTO) = this.DTOBuilder().Build(this.Insert(this.EntityBuilder().Build(dto)))
     
     override this.Update(dto : 'TDTO, [<ParamArray>] ids) = 
-        this.Update(this.EntityBuilder().Build(dto), ids)
+        let entity = this.Set().Find(ids)
+        let entry = manager.Entry(entity)
+        this.EntityBuilder().Build(dto, ref entity)
+        entry.CurrentValues.SetValues(entity)
     
     override this.Delete(_ : 'TDTO, [<ParamArray>] ids) = this.Delete(ids) |> ignore
     member this.InsertAsync(dto : 'TDTO) = async { return this.Insert(dto) } |> Async.StartAsTask
