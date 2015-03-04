b88141c5dae20bae877f31ab22f1d33b3abbe17a
Matteo Bruni <ar3s@icloud.com>
2015-03-04 22:58:43 +0100

    package update

--- Caelan.Frameworks.BIZ.NUnit/Mappers/UserDTOMapper.cs
+++ Caelan.Frameworks.BIZ.NUnit/Mappers/UserDTOMapper.cs
@@ -6,11 +6,13 @@ namespace Caelan.Frameworks.BIZ.NUnit.Mappers
 {
 	public class UserDTOMapper : DefaultMapper<User, UserDTO>
 	{
-		public override void Map(User source, UserDTO destination)
+		public override UserDTO Map(User source, UserDTO destination)
 		{
 			destination.Id = source.Id;
 			destination.Login = source.Login;
 			destination.Password = source.Password;
+
+			return destination;
 		}
 	}
 }
