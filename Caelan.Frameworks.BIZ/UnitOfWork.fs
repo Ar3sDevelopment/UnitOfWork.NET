5391ac7f15c89a560bf1df663d8525f0f335120f
Matteo Bruni <ar3s@icloud.com>
2016-02-22 09:04:48 +0100

    autofac improvements, startup

--- Caelan.Frameworks.BIZ/UnitOfWork.fs
+++ Caelan.Frameworks.BIZ/UnitOfWork.fs
@@ -1,5 +1,6 @@
 ﻿namespace Caelan.Frameworks.BIZ.Classes
 
+open Autofac
 open System
 open System.Data.Entity
 open System.Reflection
@@ -7,9 +8,15 @@ open Caelan.Frameworks.BIZ.Interfaces
 open Caelan.Frameworks.Common.Helpers
 open Caelan.Frameworks.BIZ.Modules
 
-type UnitOfWork internal (context : DbContext, autoContext) =
-    member private __.AutoContext = autoContext
-
+type UnitOfWork internal (context : DbContext, autoContext) = 
+    
+    member private __.container = 
+        let cb = ContainerBuilder()
+        cb.RegisterType<UnitOfWork>().AsSelf().AsImplementedInterfaces() |> ignore
+        cb.Build()
+    
+    member private __.autoContext = autoContext
+    
     interface IUnitOfWork with
         member this.SaveChanges() = this.SaveChanges()
         member this.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
@@ -25,41 +32,30 @@ type UnitOfWork internal (context : DbContext, autoContext) =
             this.Repository<'TEntity, 'TDTO, 'TListDTO>()
         member this.Transaction(body : Action<IUnitOfWork>) = this.Transaction(body)
         member this.TransactionSaveChanges(body : Action<IUnitOfWork>) = this.TransactionSaveChanges(body)
-
+    
     interface IDisposable with
         member this.Dispose() = this.Dispose()
     
-    abstract AfterSaveChanges : unit -> unit
-    override __.AfterSaveChanges() = ()
-    
-    member uow.SaveChanges() =
-        let res = context.SaveChanges()
-
-        if res > 0 then
-            uow.AfterSaveChanges()
-
-        res
+    member uow.SaveChanges() = context.SaveChanges()
     member __.SaveChangesAsync() = async { return! context.SaveChangesAsync() |> Async.AwaitTask } |> Async.StartAsTask
+    member this.CustomRepository<'TRepository when 'TRepository :> IRepository>() = this.GetRepository<'TRepository>()
     
-    member this.CustomRepository<'TRepository when 'TRepository :> IRepository>() = 
-        typeof<'TRepository>
-        |> MemoizeHelper.Memoize(fun tp -> 
-                                    let repository =
-                                        match this.GetType().GetProperties(BindingFlags.Instance) |> Seq.tryFind (fun t -> t.PropertyType = tp) with
-                                        | Some(repositoryProp) -> repositoryProp.GetValue(this)
-                                        | None -> Activator.CreateInstance(tp, this)
-                                    repository :?> 'TRepository)
+    member private this.GetRepository<'TRepository when 'TRepository :> IRepository>() = 
+        let mutable repository = Unchecked.defaultof<'TRepository>
+        if this.container.TryResolve<'TRepository>(&repository) |> not then 
+            let cb = ContainerBuilder()
+            cb.RegisterType<'TRepository>().AsSelf().AsImplementedInterfaces() |> ignore
+            cb.Update(this.container)
+            use scope = this.container.BeginLifetimeScope()
+            repository <- this.container.Resolve<'TRepository>()
+        repository
     
-    member private this.GetRepository<'TRepository when 'TRepository :> IRepository>()=
-        typeof<'TRepository> 
-        |> MemoizeHelper.Memoize (fun t -> t |> RepositoryReflection.FindRepositoryInAssemblies<'TRepository> [| this |])
     member this.Repository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
         this.GetRepository<IRepository<'TEntity>>()
     member this.Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct>() = 
         this.GetRepository<IRepository<'TEntity, 'TDTO>>()
     member this.Repository<'TEntity, 'TDTO, 'TListDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct and 'TListDTO : equality and 'TListDTO : null and 'TListDTO : not struct>() = 
         this.GetRepository<IListRepository<'TEntity, 'TDTO, 'TListDTO>>()
-    
     member __.Entry<'TEntity>(entity : 'TEntity) = context.Entry(entity)
     member __.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
         context.Set<'TEntity>()
@@ -83,11 +79,12 @@ type UnitOfWork internal (context : DbContext, autoContext) =
             false
     
     abstract Dispose : unit -> unit
-    override this.Dispose() = if this.AutoContext then context.Dispose()
-
-    new(context: DbContext) = new UnitOfWork(context, false)
+    
+    override this.Dispose() = 
+        if this.autoContext then context.Dispose()
+    
+    new(context : DbContext) = new UnitOfWork(context, false)
 
 type UnitOfWork<'TContext when 'TContext :> DbContext> private (context : DbContext) = 
     inherit UnitOfWork(context, true)
-    
     new() = new UnitOfWork<'TContext>(Activator.CreateInstance<'TContext>())
