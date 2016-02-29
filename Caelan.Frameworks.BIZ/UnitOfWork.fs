0fc6285af1e98c46d96bb91df994d3c0b2eb8a37
Matteo Bruni <ar3s@icloud.com>
2016-02-29 09:42:45 +0100

    refactoring and update

--- Caelan.Frameworks.BIZ/UnitOfWork.fs
+++ Caelan.Frameworks.BIZ/UnitOfWork.fs
@@ -1,15 +1,15 @@
 ﻿namespace Caelan.Frameworks.BIZ.Classes
 
 open Autofac
-open System
-open System.Collections
-open System.Linq
-open System.Data.Entity
-open System.Data.Entity.Infrastructure
-open System.Data.Entity.Core.Objects
-open System.Reflection
 open Caelan.Frameworks.BIZ.Interfaces
 open Caelan.Frameworks.Common.Helpers
+open System
+open System.Collections
+open System.Data.Entity
+open System.Data.Entity.Core.Objects
+open System.Data.Entity.Infrastructure
+open System.Linq
+open System.Reflection
 
 type UnitOfWork internal (context : DbContext, autoContext) as uow = 
     
@@ -23,8 +23,7 @@ type UnitOfWork internal (context : DbContext, autoContext) as uow =
         
         let cb = ContainerBuilder()
         cb.Register<UnitOfWork>(fun u -> uow).AsSelf().As<UnitOfWork>().AsImplementedInterfaces() |> ignore
-        cb.RegisterAssemblyTypes(assemblies).Where(fun t -> t.IsAssignableTo<IRepository>()).AsSelf()
-          .AsImplementedInterfaces() |> ignore
+        cb.RegisterAssemblyTypes(assemblies).Where(fun t -> t.IsAssignableTo<IRepository>()).AsSelf().AsImplementedInterfaces() |> ignore
         cb.Build()
     
     member private __.autoContext = autoContext
@@ -32,15 +31,11 @@ type UnitOfWork internal (context : DbContext, autoContext) as uow =
     interface IUnitOfWork with
         member this.SaveChanges() = this.SaveChanges()
         member this.AfterSaveChanges() = this.AfterSaveChanges()
-        member this.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
-            this.DbSet<'TEntity>()
+        member this.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = this.DbSet<'TEntity>()
         member this.Entry<'TEntity>(entity) = this.Entry<'TEntity>(entity)
-        member this.CustomRepository<'TRepository when 'TRepository :> IRepository>() = 
-            this.CustomRepository<'TRepository>()
-        member this.Repository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
-            this.Repository<'TEntity>()
-        member this.Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct>() = 
-            this.Repository<'TEntity, 'TDTO>()
+        member this.CustomRepository<'TRepository when 'TRepository :> IRepository>() = this.CustomRepository<'TRepository>()
+        member this.Repository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = this.Repository<'TEntity>()
+        member this.Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct>() = this.Repository<'TEntity, 'TDTO>()
         member this.Repository<'TEntity, 'TDTO, 'TListDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct and 'TListDTO : equality and 'TListDTO : null and 'TListDTO : not struct>() = 
             this.Repository<'TEntity, 'TDTO, 'TListDTO>()
         member this.Transaction(body : Action<IUnitOfWork>) = this.Transaction(body)
@@ -52,18 +47,12 @@ type UnitOfWork internal (context : DbContext, autoContext) as uow =
     member __.RegisterRepository<'TRepository when 'TRepository :> IRepository>(assemblies : Assembly []) = 
         if container.IsRegistered(typeof<'TRepository>) |> not then 
             let cb = ContainerBuilder()
-            if container.IsRegistered(typeof<Repository>) |> not then 
-                cb.RegisterType<Repository>().AsSelf().As<IRepository>() |> ignore
-            if container.IsRegistered(typedefof<Repository<_>>) |> not then 
-                cb.RegisterGeneric(typedefof<Repository<_>>).AsSelf().As(typedefof<IRepository<_>>) |> ignore
-            if container.IsRegistered(typedefof<Repository<_, _>>) |> not then 
-                cb.RegisterGeneric(typedefof<Repository<_, _>>).AsSelf().As(typedefof<IRepository<_, _>>) |> ignore
-            if container.IsRegistered(typedefof<ListRepository<_, _, _>>) |> not then 
-                cb.RegisterGeneric(typedefof<ListRepository<_, _, _>>).AsSelf().As(typedefof<IListRepository<_, _, _>>) 
-                |> ignore
+            if container.IsRegistered(typeof<Repository>) |> not then cb.RegisterType<Repository>().AsSelf().As<IRepository>().PreserveExistingDefaults() |> ignore
+            if container.IsRegistered(typedefof<Repository<_>>) |> not then cb.RegisterGeneric(typedefof<Repository<_>>).AsSelf().As(typedefof<IRepository<_>>) |> ignore
+            if container.IsRegistered(typedefof<Repository<_, _>>) |> not then cb.RegisterGeneric(typedefof<Repository<_, _>>).AsSelf().As(typedefof<IRepository<_, _>>) |> ignore
+            if container.IsRegistered(typedefof<ListRepository<_, _, _>>) |> not then cb.RegisterGeneric(typedefof<ListRepository<_, _, _>>).AsSelf().As(typedefof<IListRepository<_, _, _>>) |> ignore
             let allAssemblies = 
-                let getRefAssemblies (assembly : Assembly) = 
-                    assembly.GetReferencedAssemblies() |> Array.map Assembly.Load
+                let getRefAssemblies (assembly : Assembly) = assembly.GetReferencedAssemblies() |> Array.map Assembly.Load
                 
                 let arr = 
                     [| typeof<'TRepository>.Assembly
@@ -76,14 +65,10 @@ type UnitOfWork internal (context : DbContext, autoContext) as uow =
                 arr
                 |> Array.append (arr |> Array.collect getRefAssemblies)
                 |> Array.where (isNull >> not)
-            cb.RegisterAssemblyTypes(allAssemblies)
-              .Where(fun t -> 
-              t.IsAssignableTo<IRepository>() && t.IsInterface |> not && t.IsAbstract |> not 
-              && container.IsRegistered(t) |> not).AsSelf().AsImplementedInterfaces() |> ignore
+            cb.RegisterAssemblyTypes(allAssemblies).Where(fun t -> t.IsAssignableTo<IRepository>() && t.IsInterface |> not && t.IsAbstract |> not && container.IsRegistered(t) |> not).AsSelf().AsImplementedInterfaces() |> ignore
             if typeof<'TRepository>.IsInterface
                |> not
-               && typeof<'TRepository>.IsAbstract |> not then 
-                cb.RegisterType<'TRepository>().AsSelf().AsImplementedInterfaces() |> ignore
+               && typeof<'TRepository>.IsAbstract |> not then cb.RegisterType<'TRepository>().AsSelf().AsImplementedInterfaces() |> ignore
             cb.Update(container)
     
     member private this.GetRepository<'TRepository when 'TRepository :> IRepository>(assemblies : Assembly []) = 
@@ -148,11 +133,9 @@ type UnitOfWork internal (context : DbContext, autoContext) as uow =
     member this.SaveChangesAsync() = async { return this.SaveChanges() } |> Async.StartAsTask
     abstract AfterSaveChanges : unit -> unit
     override uow.AfterSaveChanges() = ()
-    member private uow.CallOnSaveChanges<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>(entities : obj []) = 
-        uow.Repository<'TEntity>().OnSaveChanges(entities.Cast<'TEntity>() |> Array.ofSeq)
+    member private uow.CallOnSaveChanges<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>(entities : obj []) = uow.Repository<'TEntity>().OnSaveChanges(entities.Cast<'TEntity>() |> Array.ofSeq)
     member __.Entry<'TEntity>(entity : 'TEntity) = context.Entry(entity)
-    member __.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = 
-        context.Set<'TEntity>()
+    member __.DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>() = context.Set<'TEntity>()
     
     member this.Transaction(body : Action<IUnitOfWork>) = 
         use transaction = context.Database.BeginTransaction()
