374bfb8ee473b0b989e75182af183de75a403e98
Matteo Bruni <ar3s@icloud.com>
2016-03-11 12:45:00 +0100

    Renamed project

--- Caelan.Frameworks.BIZ.NUnit/UnitOfWork.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace Caelan.Frameworks.BIZ.NUnit
-
-open Caelan.Frameworks.BIZ.Classes
-open Caelan.Frameworks.BIZ.NUnit.Data.Models
-
-type TestUnitOfWork() = 
-    inherit UnitOfWork<TestDbContext>()
-    [<DefaultValue>]
-    val mutable Users : UserRepository
