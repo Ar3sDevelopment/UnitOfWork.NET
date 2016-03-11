454d4d01952ba2b849a87faa8573e9ac6541b691
Matteo Bruni <ar3s@icloud.com>
2016-03-11 12:54:39 +0100

    missing files

---
+++ UnitOfWork.NET.NUnit/UnitOfWork.fs
@@ -0,0 +1,9 @@
+﻿namespace UnitOfWork.NET.NUnit
+
+open UnitOfWork.NET.Classes
+open UnitOfWork.NET.NUnit.Data.Models
+
+type TestUnitOfWork() = 
+    inherit UnitOfWork<TestDbContext>()
+    [<DefaultValue>]
+    val mutable Users : UserRepository
