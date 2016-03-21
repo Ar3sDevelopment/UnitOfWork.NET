ca922a44e0a01cb45107cfd76f814a83eb111d40
Matteo Bruni <ar3s@icloud.com>
2016-03-22 00:13:43 +0100

    migrated to generic code, entity framework indipendent. more projects are coming

--- UnitOfWork.NET/Interfaces.fs
+++ UnitOfWork.NET/Interfaces.fs
@@ -3,8 +3,6 @@
 open Caelan.DynamicLinq.Classes
 open System
 open System.Collections.Generic
-open System.Data.Entity
-open System.Data.Entity.Infrastructure
 open System.Linq
 open System.Linq.Expressions
 
@@ -14,126 +12,61 @@ type IRepository =
     /// </summary>
     abstract UnitOfWork : IUnitOfWork
 
-and IRepository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null> = 
+and IRepository<'T> = 
     inherit IRepository
     
     /// <summary>
     /// The DbSet used by Repository to perform operations on database.
     /// </summary>
-    abstract Set : DbSet<'TEntity>
+    abstract Data : IEnumerable<'T>
     
     /// <summary>
-    /// Recovers the entity by primary keys.
+    /// Recovers an element using a where, if multiple returned it returns the first.
     /// </summary>
-    /// <param name="ids">The primary keys values</param>
-    [<Obsolete("Use Entity instead")>]
-    abstract SingleEntity : [<ParamArray>] ids:obj [] -> 'TEntity
-    
-    /// <summary>
-    /// Recovers the entity by primary keys.
-    /// </summary>
-    /// <param name="ids">The primary keys values</param>
-    abstract Entity : [<ParamArray>] ids:obj [] -> 'TEntity
-    
-    /// <summary>
-    /// Recovers an entity using a where, if multiple returned it returns the first.
-    /// </summary>
-    /// <param name="where">The condition for retrieving the entity</param>
-    [<Obsolete("Use Entity instead")>]
-    abstract SingleEntity : where:Expression<Func<'TEntity, bool>> -> 'TEntity
-    
-    /// <summary>
-    /// Recovers an entity using a where, if multiple returned it returns the first.
-    /// </summary>
-    /// <param name="where">The condition for retrieving the entity</param>
-    abstract Entity : where:Expression<Func<'TEntity, bool>> -> 'TEntity
+    /// <param name="where">The condition for retrieving the element</param>
+    abstract Element : where:Func<'T, bool> -> 'T
     
     /// <summary>
     /// All entities of the DbSet
     /// </summary>
-    abstract All : unit -> IQueryable<'TEntity>
+    abstract All : unit -> seq<'T>
     
     /// <summary>
     /// All entities of the DbSet filtered by the where.
     /// </summary>
     /// <param name="where">The condition for filtering all entities</param>
-    abstract All : where:Expression<Func<'TEntity, bool>> -> IQueryable<'TEntity>
+    abstract All : where:Func<'T, bool> -> seq<'T>
     
     /// <summary>
-    /// Check if exists any entity satisfying the given condition.
+    /// Check if exists any element satisfying the given condition.
     /// </summary>
-    /// <param name="expr">The condition to check if any entity verifies it</param>
-    abstract Exists : expr:Expression<Func<'TEntity, bool>> -> bool
+    /// <param name="expr">The condition to check if any element verifies it</param>
+    abstract Exists : expr:Func<'T, bool> -> bool
     
     /// <summary>
     /// Counts all entities satisfying the given condition.
     /// </summary>
     /// <param name="expr">The condition for counting all entities that verify it</param>
-    abstract Count : expr:Expression<Func<'TEntity, bool>> -> int
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="entity"></param>
-    abstract Insert : entity:'TEntity -> 'TEntity
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="entity"></param>
-    /// <param name="ids"></param>
-    abstract Update : entity:'TEntity * [<ParamArray>] ids:obj [] -> unit
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="ids"></param>
-    abstract Delete : [<ParamArray>] ids:obj [] -> unit
-    
-    ///<summary>
-    ///
-    ///</summary>
-    /// <param name="entities"></param>
-    abstract OnSaveChanges : entities:IDictionary<EntityState, IEnumerable<'TEntity>> -> unit
+    abstract Count : expr:Func<'T, bool> -> int
 
-and IRepository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct> = 
-    inherit IRepository<'TEntity>
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="ids"></param>
-    [<Obsolete("Use DTO instead")>]
-    abstract SingleDTO : [<ParamArray>] ids:obj [] -> 'TDTO
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="ids"></param>
-    abstract DTO : [<ParamArray>] ids:obj [] -> 'TDTO
+and IRepository<'TSource, 'TDestination> = 
+    inherit IRepository<'TSource>
     
     /// <summary>
     /// 
     /// </summary>
     /// <param name="where"></param>
-    [<Obsolete("Use DTO instead")>]
-    abstract SingleDTO : where:Expression<Func<'TEntity, bool>> -> 'TDTO
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="where"></param>
-    abstract DTO : where:Expression<Func<'TEntity, bool>> -> 'TDTO
+    abstract ElementBuilt : where:Func<'TSource, bool> -> 'TDestination
     
     /// <summary>
     ///
     /// </summary>
-    abstract List : unit -> seq<'TDTO>
+    abstract AllBuilt : unit -> seq<'TDestination>
     
     /// <summary>
     /// 
     /// </summary>
-    abstract List : Expression<Func<'TEntity, bool>> -> seq<'TDTO>
+    abstract AllBuilt : Func<'TSource, bool> -> seq<'TDestination>
     
     /// <summary>
     /// 
@@ -143,38 +76,14 @@ and IRepository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equali
     /// <param name="sort"></param>
     /// <param name="filter"></param>
     /// <param name="where"></param>
-    [<Obsolete("Use DataSource instead")>]
-    abstract All : take:int * skip:int * sort:ICollection<Sort> * filter:Filter * where:Expression<Func<'TEntity, bool>> -> DataSourceResult<'TDTO>
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="take"></param>
-    /// <param name="skip"></param>
-    /// <param name="sort"></param>
-    /// <param name="filter"></param>
-    /// <param name="where"></param>
-    abstract DataSource : take:int * skip:int * sort:ICollection<Sort> * filter:Filter * where:Expression<Func<'TEntity, bool>> -> DataSourceResult<'TDTO>
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="dto"></param>
-    abstract Insert : dto:'TDTO -> 'TDTO
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="dto"></param>
-    /// <param name="ids"></param>
-    abstract Update : dto:'TDTO * [<ParamArray>] ids:obj [] -> unit
+    abstract DataSource : take:int * skip:int * sort:ICollection<Sort> * filter:Filter * where:Func<'TSource, bool> -> DataSourceResult<'TDestination>
 
-and IListRepository<'TEntity, 'TDTO, 'TListDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct and 'TListDTO : equality and 'TListDTO : null and 'TListDTO : not struct> = 
-    inherit IRepository<'TEntity, 'TDTO>
+and IListRepository<'TSource, 'TDestination, 'TListDestination> = 
+    inherit IRepository<'TSource, 'TDestination>
     /// <summary>
     /// 
     /// </summary>
-    abstract ListRepository : IRepository<'TEntity, 'TListDTO> with get, set
+    abstract ListRepository : IRepository<'TSource, 'TListDestination> with get, set
 
 and IUnitOfWork = 
     inherit IDisposable
@@ -182,28 +91,12 @@ and IUnitOfWork =
     /// <summary>
     /// 
     /// </summary>
-    abstract BeforeSaveChanges : context:DbContext -> unit
+    abstract SaveChanges : unit -> unit
     
     /// <summary>
     /// 
     /// </summary>
-    abstract SaveChanges : unit -> int
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    abstract AfterSaveChanges : context:DbContext -> unit
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="entity"></param>
-    abstract Entry<'TEntity> : entity:'TEntity -> DbEntityEntry
-    
-    /// <summary>
-    /// 
-    /// </summary>
-    abstract DbSet<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null> : unit -> DbSet<'TEntity>
+    abstract Data<'T> : unit -> IEnumerable<'T>
     
     /// <summary>
     /// 
@@ -215,7 +108,7 @@ and IUnitOfWork =
     /// 
     /// </summary>
     /// <param name="body"></param>
-    abstract TransactionSaveChanges : body:Action<IUnitOfWork> -> bool
+    abstract TransactionSaveChanges : body:Action<IUnitOfWork> -> unit
     
     /// <summary>
     /// 
@@ -225,15 +118,14 @@ and IUnitOfWork =
     /// <summary>
     /// 
     /// </summary>
-    abstract Repository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null> : unit -> IRepository<'TEntity>
+    abstract Repository<'T> : unit -> IRepository<'T>
     
     /// <summary>
     /// 
     /// </summary>
-    abstract Repository<'TEntity, 'TDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct> : unit -> IRepository<'TEntity, 'TDTO>
+    abstract Repository<'TSource, 'TDestination> : unit -> IRepository<'TSource, 'TDestination>
     
     /// <summary>
     /// 
     /// </summary>
-    abstract Repository<'TEntity, 'TDTO, 'TListDTO when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null and 'TDTO : equality and 'TDTO : null and 'TDTO : not struct and 'TListDTO : equality and 'TListDTO : null and 'TListDTO : not struct> : unit
-     -> IListRepository<'TEntity, 'TDTO, 'TListDTO>
+    abstract Repository<'TSource, 'TDestination, 'TListDestination> : unit -> IListRepository<'TSource, 'TDestination, 'TListDestination>
--- UnitOfWork.NET/RepositoryEntity.fs
+++ UnitOfWork.NET/RepositoryEntity.fs
@@ -2,60 +2,26 @@
 
 open System
 open System.Collections.Generic
-open System.Data.Entity
 open System.Linq
 open System.Linq.Expressions
 open UnitOfWork.NET.Interfaces
 
-type Repository<'TEntity when 'TEntity : not struct and 'TEntity : equality and 'TEntity : null>(manager) = 
+[<AbstractClass>]
+type Repository<'T>(manager) = 
     inherit Repository(manager)
     
-    interface IRepository<'TEntity> with
-        member this.Set = this.Set
-        member this.Entity([<ParamArray>] ids : obj []) = this.Entity ids
-        member this.Entity(expr : Expression<Func<'TEntity, bool>>) = this.Entity expr
-        member this.SingleEntity([<ParamArray>] ids : obj []) = this.Entity ids
-        member this.SingleEntity(expr : Expression<Func<'TEntity, bool>>) = this.Entity expr
+    interface IRepository<'T> with
+        member this.Data = this.Data
+        member this.Element(expr : Func<'T, bool>) = this.Element expr
         member this.All() = this.All()
         member this.All whereExpr = whereExpr |> this.All
-        member this.Insert(entity : 'TEntity) = entity |> this.Insert
-        member this.Update(entity : 'TEntity, [<ParamArray>] ids) = this.Update(entity, ids)
-        member this.Delete([<ParamArray>] ids : obj []) = this.Delete ids
         member this.Exists expr = expr |> this.Exists
         member this.Count expr = expr |> this.Count
-        member this.OnSaveChanges(entities : IDictionary<EntityState, IEnumerable<'TEntity>>) = this.OnSaveChanges entities
     
-    member __.Set = manager.DbSet<'TEntity>()
-    member this.Entity([<ParamArray>] ids) = this.Set.Find ids
-    member this.Entity expr = expr |> this.Set.FirstOrDefault
-    
-    [<Obsolete("Use Entity instead")>]
-    member this.SingleEntity([<ParamArray>] ids : obj []) = this.Entity ids
-    
-    [<Obsolete("Use Entity instead")>]
-    member this.SingleEntity(expr : Expression<Func<'TEntity, bool>>) = this.Entity expr
-    
-    member this.All() = this.Set.AsQueryable()
-    member this.All(whereExpr : Expression<Func<'TEntity, bool>>) = this.Set.Where(whereExpr).AsQueryable()
-    member this.Exists expr = this.Set.Any expr
-    member this.Count expr = this.Set.Count expr
-    abstract OnSaveChanges : entities:IDictionary<EntityState, IEnumerable<'TEntity>> -> unit
-    override this.OnSaveChanges(entities : IDictionary<EntityState, IEnumerable<'TEntity>>) = ()
-    abstract Insert : entity:'TEntity -> 'TEntity
-    abstract Update : 'TEntity * [<ParamArray>] ids:obj [] -> unit
-    abstract Delete : [<ParamArray>] ids:obj [] -> unit
-    override this.Insert entity = entity |> this.Set.Add
-    override this.Update(entity, [<ParamArray>] ids) = manager.Entry(ids |> this.Entity).CurrentValues.SetValues(entity)
-    
-    override this.Delete([<ParamArray>] ids) = 
-        ids
-        |> this.Entity
-        |> this.Set.Remove
-        |> ignore
-    
-    member this.InsertAsync entity = async { return this.Insert(entity) } |> Async.StartAsTask
-    member this.UpdateAsync(entity, ids) = async { this.Update(entity, ids) } |> Async.StartAsTask
-    member this.DeleteAsync(entity, [<ParamArray>] ids) = async { this.Delete(entity, ids) } |> Async.StartAsTask
-    member this.DeleteAsync(ids) = async { this.Delete(ids) } |> Async.StartAsTask
-    member this.SingleEntityAsync([<ParamArray>] ids : obj []) = async { return this.Entity(ids) } |> Async.StartAsTask
-    member this.SingleEntityAsync(expr : Expression<Func<'TEntity, bool>>) = async { return this.Entity(expr) } |> Async.StartAsTask
+    member __.Data = manager.Data<'T>()
+    member this.Element expr = expr |> this.Data.FirstOrDefault
+    member this.All() = this.Data
+    member this.All(whereExpr : Func<'T, bool>) = this.Data.Where(whereExpr)
+    member this.Exists expr = this.Data.Any expr
+    member this.Count expr = this.Data.Count expr
+    member this.ElementAsync(expr : Func<'T, bool>) = async { return this.Element(expr) } |> Async.StartAsTask
