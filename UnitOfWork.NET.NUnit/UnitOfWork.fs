348f5741fabaf2ac324cca8484c9e33cb02d3100
Matteo Bruni <matteo.bruni@me.com>
2016-03-22 09:28:21 +0100

    tests

--- UnitOfWork.NET.NUnit/UnitOfWork.fs
+++ UnitOfWork.NET.NUnit/UnitOfWork.fs
@@ -13,10 +13,11 @@ type TestUnitOfWork() =
     override __.Data<'T>() =
         let dataType = typeof<'T>
         let rand = Random(int(DateTime.Now.Ticks))
+        let count = rand.Next() % 10 + 1
         if dataType = typeof<int> then
-            Seq.init(rand.Next() % 10 + 1) (fun t -> rand.Next()) :?> seq<'T>
+            Seq.init(count) (fun t -> rand.Next()) :?> seq<'T>
         else if dataType = typeof<double> then
-            Seq.init(rand.Next() % 10 + 1) (fun t -> double(rand.NextDouble())) :?> seq<'T>
+            Seq.init(count) (fun t -> double(rand.NextDouble())) :?> seq<'T>
         else if dataType = typeof<float> then
-            Seq.init(rand.Next() % 10 + 1) (fun t -> rand.NextDouble()) :?> seq<'T>
+            Seq.init(count) (fun t -> rand.NextDouble()) :?> seq<'T>
         else base.Data<'T>()
