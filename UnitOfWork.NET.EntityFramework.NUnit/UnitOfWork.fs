504001e84a8ab85bc84ba13501be2d3866bed393
Matteo Bruni <matteo.bruni@me.com>
2016-03-22 14:07:29 +0100

    reimported unit tests for ef

---
+++ UnitOfWork.NET.EntityFramework.NUnit/UnitOfWork.fs
@@ -0,0 +1,9 @@
+﻿namespace UnitOfWork.NET.EntityFramework.NUnit
+
+open UnitOfWork.NET.EntityFramework.Classes
+open UnitOfWork.NET.EntityFramework.NUnit.Data.Models
+
+type TestUnitOfWork() = 
+    inherit EntityUnitOfWork<TestDbContext>()
+    [<DefaultValue>]
+    val mutable Users : UserRepository
