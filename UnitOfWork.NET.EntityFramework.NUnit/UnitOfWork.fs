80094e979bff1932e790a159de77e98b746af0df
Matteo Bruni <ar3s@icloud.com>
2016-04-22 16:11:41 +0200

    Removed EF projects, moved to other repository

--- UnitOfWork.NET.EntityFramework.NUnit/UnitOfWork.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace UnitOfWork.NET.EntityFramework.NUnit
-
-open UnitOfWork.NET.EntityFramework.Classes
-open UnitOfWork.NET.EntityFramework.NUnit.Data.Models
-
-type TestUnitOfWork() = 
-    inherit EntityUnitOfWork<TestDbContext>()
-    [<DefaultValue>]
-    val mutable Users : UserRepository
