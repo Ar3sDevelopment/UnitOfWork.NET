6ac9181360a2ff4482c672ca76d0c11b49b87aff
Matteo Bruni <ar3s@icloud.com>
2016-09-30 09:37:22 +0200

    fix uow

--- UnitOfWork.NET/Classes/UnitOfWork.cs
+++ UnitOfWork.NET/Classes/UnitOfWork.cs
@@ -9,162 +9,182 @@ using UnitOfWork.NET.Extenders;
 
 namespace UnitOfWork.NET.Classes
 {
-    public class UnitOfWork : IUnitOfWork
-    {
-        private readonly ObservableCollection<Assembly> _assemblies;
-        private readonly IContainer _container;
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly ObservableCollection<Assembly> _assemblies;
+		private readonly IContainer _container;
 
-        public UnitOfWork()
-        {
-            _assemblies = new ObservableCollection<Assembly>();
+		public UnitOfWork()
+		{
+			_assemblies = new ObservableCollection<Assembly>();
 
-            var cb = new ContainerBuilder();
+			var cb = new ContainerBuilder();
 
-            cb.Register(t => this).AsImplementedInterfaces().AsSelf().As<UnitOfWork>();
-            cb.RegisterType<Repository>().AsSelf().As<IRepository>().PreserveExistingDefaults();
-            cb.RegisterGeneric(typeof(Repository<>)).AsSelf().As(typeof(IRepository<>));
-            cb.RegisterGeneric(typeof(Repository<,>)).AsSelf().As(typeof(IRepository<,>));
-            cb.RegisterGeneric(typeof(Repository<,,>)).AsSelf().As(typeof(IListRepository<,,>));
+			cb.Register(t => this).AsImplementedInterfaces().AsSelf().As<UnitOfWork>();
+			cb.RegisterType<Repository>().AsSelf().As<IRepository>().PreserveExistingDefaults();
+			cb.RegisterGeneric(typeof(Repository<>)).AsSelf().As(typeof(IRepository<>));
+			cb.RegisterGeneric(typeof(Repository<,>)).AsSelf().As(typeof(IRepository<,>));
+			cb.RegisterGeneric(typeof(Repository<,,>)).AsSelf().As(typeof(IListRepository<,,>));
 
-            _container = cb.Build();
+			_container = cb.Build();
 
-            cb = new ContainerBuilder();
+			cb = new ContainerBuilder();
 
-            var fields = GetType().GetFields().ToArray();
-            var properties = GetType().GetProperties().ToArray();
+			var fields = GetType().GetFields().ToArray();
+			var properties = GetType().GetProperties().ToArray();
 
-            foreach (var type in fields.Select(t => t.FieldType).Union(properties.Select(t => t.PropertyType)).Where(IsRepository)) cb.RegisterType(type).AsSelf().AsImplementedInterfaces();
+			foreach (var type in fields.Select(t => t.FieldType).Union(properties.Select(t => t.PropertyType)).Where(IsRepository)) cb.RegisterType(type).AsSelf().AsImplementedInterfaces();
 
-            cb.Update(_container);
+			cb.Update(_container);
 
-            _assemblies.CollectionChanged += (sender, args) =>
-            {
-                RegisterAssembly(args.NewItems.Cast<Assembly>().ToArray());
+			_assemblies.CollectionChanged += (sender, args) =>
+			{
+				RegisterAssembly(args.NewItems.Cast<Assembly>().ToArray());
 
-                UpdateProperties();
-            };
+				UpdateProperties();
+			};
 
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) _assemblies.Add(assembly);
-        }
+			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) _assemblies.Add(assembly);
+		}
 
-        protected void UpdateProperties()
-        {
-            var fields = GetType().GetFields().ToArray();
-            var properties = GetType().GetProperties().ToArray();
+		protected void UpdateProperties()
+		{
+			var fields = GetType().GetFields().ToArray();
+			var properties = GetType().GetProperties().ToArray();
 
-            foreach (var field in fields.Where(t => t.FieldType.IsAssignableTo<IRepository>())) field.SetValue(this, _container.ResolveOptional(field.FieldType));
-            foreach (var property in properties.Where(t => t.PropertyType.IsAssignableTo<IRepository>())) property.SetValue(this, _container.ResolveOptional(property.PropertyType));
-        }
+			foreach (var field in fields.Where(t => t.FieldType.IsAssignableTo<IRepository>())) field.SetValue(this, _container.ResolveOptional(field.FieldType));
+			foreach (var property in properties.Where(t => t.PropertyType.IsAssignableTo<IRepository>())) property.SetValue(this, _container.ResolveOptional(property.PropertyType));
+		}
 
-        private bool IsRepository(Type t)
-        {
-            try
-            {
-                return t.IsAssignableTo<IRepository>() && !t.IsInterface && !t.IsAbstract && t != typeof(Repository) && ((t.IsGenericType && t.GetGenericTypeDefinition() != typeof(Repository<>) && t.GetGenericTypeDefinition() != typeof(Repository<,>) && t.GetGenericTypeDefinition() != typeof(Repository<,,>)) || !t.IsGenericType) && !_container.IsRegistered(t);
-            }
-            catch
-            {
-                return false;
-            }
-        }
+		private bool IsRepository(Type t)
+		{
+			try
+			{
+				return t.IsAssignableTo<IRepository>() && !t.IsInterface && !t.IsAbstract && t != typeof(Repository) && ((t.IsGenericType && t.GetGenericTypeDefinition() != typeof(Repository<>) && t.GetGenericTypeDefinition() != typeof(Repository<,>) && t.GetGenericTypeDefinition() != typeof(Repository<,,>)) || !t.IsGenericType) && !_container.IsRegistered(t);
+			}
+			catch
+			{
+				return false;
+			}
+		}
 
-        private void RegisterAssembly(Assembly[] assemblyArr)
-        {
-            var cb = new ContainerBuilder();
+		private void RegisterAssembly(Assembly[] assemblyArr)
+		{
+			var cb = new ContainerBuilder();
 
-            cb.RegisterAssemblyTypes(assemblyArr.Where(t =>
-            {
-                try
-                {
-                    return t.GetTypes().Any(IsRepository);
-                }
-                catch
-                {
-                    return false;
-                }
-            }).ToArray()).Where(IsRepository).AsSelf().AsImplementedInterfaces();
-            cb.Update(_container);
+			cb.RegisterAssemblyTypes(assemblyArr.Where(t =>
+			{
+				try
+				{
+					return t.GetTypes().Any(IsRepository);
+				}
+				catch
+				{
+					return false;
+				}
+			}).ToArray()).Where(IsRepository).AsSelf().AsImplementedInterfaces();
+			cb.Update(_container);
 
-            foreach (var assembly in assemblyArr.SelectMany(t => t.GetReferencedAssemblies()).Select(t =>
-            {
-                try
-                {
-                    return Assembly.Load(t);
-                }
-                catch
-                {
-                    return null;
-                }
-            }).Where(
-                t =>
-                {
-                    try
-                    {
-                        return t != null && !_assemblies.Contains(t) && t.GetTypes().Any(IsRepository);
-                    }
-                    catch
-                    {
-                        return false;
-                    }
-                }))
-                _assemblies.Add(assembly);
-        }
+			foreach (var assembly in assemblyArr.SelectMany(t => t.GetReferencedAssemblies()).Select(t =>
+			{
+				try
+				{
+					return Assembly.Load(t);
+				}
+				catch
+				{
+					return null;
+				}
+			}).Where(
+				t =>
+				{
+					try
+					{
+						return t != null && !_assemblies.Contains(t) && t.GetTypes().Any(IsRepository);
+					}
+					catch
+					{
+						return false;
+					}
+				}))
+				_assemblies.Add(assembly);
+		}
 
-        public void RegisterRepository<TRepository>() where TRepository : IRepository => RegisterRepository(typeof(TRepository));
+		public void RegisterRepository<TRepository>() where TRepository : IRepository => RegisterRepository(typeof(TRepository));
 
-        public void RegisterRepositories(Type[] repositoryTypes)
-        {
-            var cb = new ContainerBuilder();
+		public void RegisterRepositories(Type[] repositoryTypes)
+		{
+			var cb = new ContainerBuilder();
 
-            foreach (var repositoryType in repositoryTypes)
-            {
-                if (repositoryType.IsInterface || repositoryType.IsAbstract || IsRepositoryRegistered(repositoryType)) return;
-                RegisterRepository(cb, repositoryType);
-            }
+			foreach (var repositoryType in repositoryTypes)
+			{
+				if (repositoryType.IsInterface || repositoryType.IsAbstract || IsRepositoryRegistered(repositoryType)) return;
+				RegisterRepository(cb, repositoryType);
+			}
 
-            UpdateContainer(cb);
-        }
+			UpdateContainer(cb);
+		}
 
-        protected virtual void RegisterRepository(ContainerBuilder cb, Type repositoryType)
-        {
-            if (repositoryType.IsGenericTypeDefinition)
-                cb.RegisterGeneric(repositoryType).AsSelf().AsRepository().AsImplementedInterfaces();
-            else
-                cb.RegisterType(repositoryType).AsSelf().AsRepository().AsImplementedInterfaces();
-        }
+		protected virtual void RegisterRepository(ContainerBuilder cb, Type repositoryType)
+		{
+			if (repositoryType.IsGenericTypeDefinition)
+			{
+				cb.RegisterGeneric(repositoryType).AsSelf().AsRepository().AsImplementedInterfaces();
+			}
+			else
+			{
+				var baseType = repositoryType.BaseType;
+				while (!baseType.ContainsGenericParameters)
+				{
+					baseType = repositoryType.BaseType;
+				}
+				var args = baseType.GenericTypeArguments;
+				Type sourceType = null;
+				Type destType = null;
+				if (args.Length > 0)
+				{
+					sourceType = args.First();
+					if (args.Length > 1)
+					{
+						destType = args.Skip(1).First();
+					}
+				}
+				cb.RegisterType(repositoryType).AsSelf().AsRepository(sourceType, destType).AsImplementedInterfaces();
+			}
+		}
 
-        public void RegisterRepository(Type repositoryType)
-        {
-            RegisterRepositories(new[] { repositoryType });
-        }
+		public void RegisterRepository(Type repositoryType)
+		{
+			RegisterRepositories(new[] { repositoryType });
+		}
 
-        private TRepository GetRepository<TRepository>() where TRepository : IRepository
-        {
-            RegisterRepository<TRepository>();
+		private TRepository GetRepository<TRepository>() where TRepository : IRepository
+		{
+			RegisterRepository<TRepository>();
 
-            return _container.Resolve<TRepository>();
-        }
+			return _container.Resolve<TRepository>();
+		}
 
-        protected void UpdateContainer(ContainerBuilder cb) => cb.Update(_container);
+		protected void UpdateContainer(ContainerBuilder cb) => cb.Update(_container);
 
-        protected bool IsRepositoryRegistered<TRepository>() where TRepository : IRepository => IsRepositoryRegistered(typeof(TRepository));
+		protected bool IsRepositoryRegistered<TRepository>() where TRepository : IRepository => IsRepositoryRegistered(typeof(TRepository));
 
-        protected bool IsRepositoryRegistered(Type repositoryType) => _container.IsRegistered(repositoryType);
+		protected bool IsRepositoryRegistered(Type repositoryType) => _container.IsRegistered(repositoryType);
 
-        public TRepository CustomRepository<TRepository>() where TRepository : IRepository => GetRepository<TRepository>();
+		public TRepository CustomRepository<TRepository>() where TRepository : IRepository => GetRepository<TRepository>();
 
-        public IRepository<T> Repository<T>() where T : class, new() => GetRepository<IRepository<T>>();
+		public IRepository<T> Repository<T>() where T : class, new() => GetRepository<IRepository<T>>();
 
-        public IRepository<TSource, TDestination> Repository<TSource, TDestination>() where TSource : class, new() where TDestination : class, new() => GetRepository<IRepository<TSource, TDestination>>();
+		public IRepository<TSource, TDestination> Repository<TSource, TDestination>() where TSource : class, new() where TDestination : class, new() => GetRepository<IRepository<TSource, TDestination>>();
 
-        public IListRepository<TSource, TDestination, TListDestination> Repository<TSource, TDestination, TListDestination>() where TSource : class, new() where TDestination : class, new() where TListDestination : class, new() => GetRepository<IListRepository<TSource, TDestination, TListDestination>>();
+		public IListRepository<TSource, TDestination, TListDestination> Repository<TSource, TDestination, TListDestination>() where TSource : class, new() where TDestination : class, new() where TListDestination : class, new() => GetRepository<IListRepository<TSource, TDestination, TListDestination>>();
 
-        public virtual IQueryable<T> Data<T>() where T : class, new() => Enumerable.Empty<T>().AsQueryable();
+		public virtual IQueryable<T> Data<T>() where T : class, new() => Enumerable.Empty<T>().AsQueryable();
 
-        public virtual void Dispose()
-        {
-            _container.Dispose();
-        }
-    }
+		public virtual void Dispose()
+		{
+			_container.Dispose();
+		}
+	}
 }
 
--- UnitOfWork.NET/Extenders/AutofacExtender.cs
+++ UnitOfWork.NET/Extenders/AutofacExtender.cs
@@ -8,37 +8,37 @@ using UnitOfWork.NET.Interfaces;
 
 namespace UnitOfWork.NET.Extenders
 {
-    internal static class AutofacExtender
-    {
-        public static IRegistrationBuilder<TLimit, ReflectionActivatorData, DynamicRegistrationStyle> AsRepository<TLimit>(this IRegistrationBuilder<TLimit, ReflectionActivatorData, DynamicRegistrationStyle> registration, Type sourceType = null, Type destinationType = null)
-        {
-            var res = registration.As<IRepository>();
+	internal static class AutofacExtender
+	{
+		public static IRegistrationBuilder<TLimit, ReflectionActivatorData, DynamicRegistrationStyle> AsRepository<TLimit>(this IRegistrationBuilder<TLimit, ReflectionActivatorData, DynamicRegistrationStyle> registration, Type sourceType = null, Type destinationType = null)
+		{
+			var res = registration.As<IRepository>();
 
-            if (sourceType != null)
-            {
-                res = res.As(typeof(IRepository<>).MakeGenericType(sourceType));
+			if (sourceType != null)
+			{
+				res = res.As(typeof(IRepository<>).MakeGenericType(sourceType));
 
-                if (destinationType != null)
-                    res = res.As(typeof(IRepository<,>).MakeGenericType(sourceType, destinationType));
-            }
+				if (destinationType != null)
+					res = res.As(typeof(IRepository<,>).MakeGenericType(sourceType, destinationType));
+			}
 
-            return res;
-        }
+			return res;
+		}
 
 
-        public static IRegistrationBuilder<TLimit, TConcreteActivatorData, SingleRegistrationStyle> AsRepository<TLimit, TConcreteActivatorData>(this IRegistrationBuilder<TLimit, TConcreteActivatorData, SingleRegistrationStyle> registration, Type sourceType = null, Type destinationType = null) where TConcreteActivatorData : IConcreteActivatorData
-        {
-            var res = registration.As<IRepository>();
+		public static IRegistrationBuilder<TLimit, TConcreteActivatorData, SingleRegistrationStyle> AsRepository<TLimit, TConcreteActivatorData>(this IRegistrationBuilder<TLimit, TConcreteActivatorData, SingleRegistrationStyle> registration, Type sourceType = null, Type destinationType = null) where TConcreteActivatorData : IConcreteActivatorData
+		{
+			var res = registration.As<IRepository>();
 
-            if (sourceType != null)
-            {
-                res = res.As(typeof(IRepository<>).MakeGenericType(sourceType));
+			if (sourceType != null)
+			{
+				res = res.As(typeof(IRepository<>).MakeGenericType(sourceType));
 
-                if (destinationType != null)
-                    res = res.As(typeof(IRepository<,>).MakeGenericType(sourceType, destinationType));
-            }
+				if (destinationType != null)
+					res = res.As(typeof(IRepository<,>).MakeGenericType(sourceType, destinationType));
+			}
 
-            return res;
-        }
-    }
+			return res;
+		}
+	}
 }
